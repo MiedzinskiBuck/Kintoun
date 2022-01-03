b98c8b7da2df4356d10cd0e454c69899cc3e030e
Andre Buck Miedzinski <andre_bm90@hotmail.com>
2022-01-03 18:29:07 -0300

    Started assume role enumeration. I'm still working on how will I return the results

---
+++ modules/enumeration/iam_enumerate_assume_role.py
@@ -0,0 +1,22 @@
+import boto3
+from colorama import Fore, Style
+from modules.enumeration import iam_enumerate_permissions
+
+def create_client(botoconfig, session):
+    client = session.client('iam', config=botoconfig)
+    return client
+
+def help():
+    print("[+] This module will enumerate the assume role policy for the existing roles.")
+    print("[+] It will take advantage os the 'iam_enumerate_permissions' module.")
+
+def main(botoconfig, session):
+    print("\n[+] Starting enumeration of Assume Role Policies on the account...")
+    client = create_client(botoconfig, session)
+    user_details, group_details, role_details, policy_details = iam_enumerate_permissions.get_account_information(client)
+    print("[+] Parsing results...")
+    for role in role_details:
+        if role['AssumeRolePolicyDocument']:
+            print("\n[+] Role " + Fore.GREEN + "{}".format(role['Arn']) + Style.RESET_ALL + " can be assumed by:")
+            for principal in role['AssumeRolePolicyDocument']['Statement']:
+                print(Fore.YELLOW + "\t{}".format(principal['Principal']) + Style.RESET_ALL)
