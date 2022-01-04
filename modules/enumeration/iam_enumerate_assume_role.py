83cada16a4647b2161b005ee8ba5f7e58a5b41c4
Andre Buck Miedzinski <andre_bm90@hotmail.com>
2022-01-04 09:47:23 -0300

    Finished Assume Role Enumeration

--- modules/enumeration/iam_enumerate_assume_role.py
+++ modules/enumeration/iam_enumerate_assume_role.py
@@ -15,8 +15,14 @@ def main(botoconfig, session):
     client = create_client(botoconfig, session)
     user_details, group_details, role_details, policy_details = iam_enumerate_permissions.get_account_information(client)
     print("[+] Parsing results...")
+
+    role_results = {}
+
     for role in role_details:
         if role['AssumeRolePolicyDocument']:
             print("\n[+] Role " + Fore.GREEN + "{}".format(role['Arn']) + Style.RESET_ALL + " can be assumed by:")
             for principal in role['AssumeRolePolicyDocument']['Statement']:
                 print(Fore.YELLOW + "\t{}".format(principal['Principal']) + Style.RESET_ALL)
+                role_results[role['Arn']] = principal['Principal']
+
+    return role_results
