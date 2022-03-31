e2b812256de4c7c794473cc976baceceafa715fc
André Buck <andre_bm90@hotmail.com>
2022-03-31 20:13:07 -0300

    Created module to enumerate parameters from system manager

---
+++ functions/region_parser.py
@@ -0,0 +1,24 @@
+
+class Region():
+    def region_select(self):
+        regions_file = open("data/regions.txt", "r")
+        regions = regions_file.read().splitlines()
+        print("[+] Available Regions...\n")
+
+        for region in regions:
+            print("- {}".format(region))
+        selected_region = input("\n[+] Select region (Default All): ")
+
+        if not selected_region:
+            selected_regions = []
+            for region in regions:
+                selected_regions.append(region)
+            return selected_regions
+
+        elif selected_region not in regions:
+            print("[-] Invalid Region...")
+            return False
+
+        else:
+            selected_region = [selected_region]
+            return selected_region
