422d4bc2ab20c0618e7481519cfb0e6df7bc6de3
André Buck Miedzinski <andre_bm90@hotmail.com>
2023-11-17 12:14:39 -0300

    Finished EC2 bf. Need to refactor code so it can be more efficient

--- functions/ec2_handler.py
+++ functions/ec2_handler.py
@@ -31,3 +31,73 @@ class EC2():
         snapshots = self.client.describe_snapshots()
 
         return snapshots
+
+    def describe_account_attributes(self):
+        attributes = self.client.describe_account_attributes()
+
+        return attributes
+
+    def describe_addresses(self):
+        addresses = self.client.describe_addresses()
+
+        return addresses
+
+    def describe_availability_zones(self):
+        zones = self.client.describe_availability_zones()
+
+        return zones
+
+    def describe_client_vpn_endpoints(self):
+        endpoints = self.client.describe_client_vpn_endpoints()
+
+        return endpoints 
+
+    def describe_images(self):
+        images = self.client.describe_images()
+
+        return images 
+
+    def describe_instance_status(self):
+        status = self.client.describe_instance_status()
+
+        return status 
+
+    def describe_internet_gateways(self):
+        gateways = self.client.describe_internet_gateways()
+
+        return gateways
+
+    def describe_key_pairs(self):
+        keys = self.client.describe_key_pairs()
+
+        return keys 
+
+    def describe_launch_templates(self):
+        templates = self.client.describe_launch_templates()
+
+        return templates
+
+    def describe_network_acls(self):
+        acls = self.client.describe_network_acls()
+
+        return acls
+
+    def describe_security_groups(self):
+        groups = self.client.describe_security_groups()
+
+        return groups 
+
+    def describe_subnets(self):
+        subnets = self.client.describe_subnets()
+
+        return subnets
+
+    def describe_tags(self):
+        tags = self.client.describe_tags()
+
+        return tags
+
+    def describe_volumes(self):
+        volumes = self.client.describe_volumes()
+
+        return volumes
--- modules/enumeration/recon_infogather.py
+++ modules/enumeration/recon_infogather.py
@@ -42,17 +42,177 @@ def main(botoconfig, session):
     regions = regions_file.read().splitlines()
 
     # Describe Instances
-    for region in regions:
-        ec2 = ec2_handler.EC2(botoconfig, session, region)
-        describe_instances = ec2.describe_instances()
-        if describe_instances:
-            print(f"\t{Fore.GREEN}- ec2.describe_intances{Style.RESET_ALL}")
-            break
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_instances = ec2.describe_instances()
+            if describe_instances:
+                print(f"\t{Fore.GREEN}- ec2.describe_intances{Style.RESET_ALL}")
+                break
+    except:
+        pass
     
     # Describe Snapshots 
-    for region in regions:
-        ec2 = ec2_handler.EC2(botoconfig, session, region)
-        describe_snapshots = ec2.describe_snapshots()
-        if describe_snapshots:
-            print(f"\t{Fore.GREEN}- ec2.describe_snapshots{Style.RESET_ALL}")
-            break
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_snapshots = ec2.describe_snapshots()
+            if describe_snapshots:
+                print(f"\t{Fore.GREEN}- ec2.describe_snapshots{Style.RESET_ALL}")
+                break
+    except:
+        pass
+
+    # Describe Attributes 
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_attributes = ec2.describe_account_attributes()
+            if describe_attributes:
+                print(f"\t{Fore.GREEN}- ec2.describe_account_attributes{Style.RESET_ALL}")
+                break
+    except:
+        pass
+
+    # Describe Addresses
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_addresses = ec2.describe_addresses()
+            if describe_addresses :
+                print(f"\t{Fore.GREEN}- ec2.describe_addresses{Style.RESET_ALL}")
+                break
+    except:
+        pass
+
+    # Describe Availability Zones
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_zones = ec2.describe_availability_zones()
+            if describe_zones:
+                print(f"\t{Fore.GREEN}- ec2.describe_availability_zones{Style.RESET_ALL}")
+                break
+    except:
+        pass
+
+    # Describe VPN endpoints
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_vpn= ec2.describe_client_vpn_endpoints()
+            if describe_vpn:
+                print(f"\t{Fore.GREEN}- ec2.describe_client_vpn_endpoints{Style.RESET_ALL}")
+                break
+    except:
+        pass
+
+    # Describe Images
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_images = ec2.describe_images()
+            if describe_images:
+                print(f"\t{Fore.GREEN}- ec2.describe_images{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Instance Status
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_status = ec2.describe_instance_status()
+            if describe_status :
+                print(f"\t{Fore.GREEN}- ec2.describe_instance_status{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Internet Gateways
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_gateways = ec2.describe_internet_gateways()
+            if describe_gateways:
+                print(f"\t{Fore.GREEN}- ec2.describe_internet_gateways{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Key Pairs
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_keys = ec2.describe_key_pairs()
+            if describe_keys:
+                print(f"\t{Fore.GREEN}- ec2.describe_key_pairs{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Launch Templates
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_templates = ec2.describe_launch_templates()
+            if describe_templates:
+                print(f"\t{Fore.GREEN}- ec2.describe_launch_templates{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Network ACLS
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_acls = ec2.describe_network_acls()
+            if describe_acls:
+                print(f"\t{Fore.GREEN}- ec2.describe_network_acls{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Security Groups
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_groups = ec2.describe_security_groups()
+            if describe_groups:
+                print(f"\t{Fore.GREEN}- ec2.describe_security_groups{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Subnets
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_subnets = ec2.describe_subnets()
+            if describe_subnets:
+                print(f"\t{Fore.GREEN}- ec2.describe_subnets{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Tags 
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_tags = ec2.describe_tags()
+            if describe_tags:
+                print(f"\t{Fore.GREEN}- ec2.describe_tags{Style.RESET_ALL}")
+                break
+    except:
+        pass
+    
+    # Describe Volumes
+    try:
+        for region in regions:
+            ec2 = ec2_handler.EC2(botoconfig, session, region)
+            describe_volumes = ec2.describe_volumes()
+            if describe_volumes:
+                print(f"\t{Fore.GREEN}- ec2.describe_volumes{Style.RESET_ALL}")
+                break
+    except:
+        pass
